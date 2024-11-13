afba3e78733b87fbab7827d55c8a359a36ebb4c1
luanaliriel <luana.bsantos@sptech.school>
2024-11-12 23:41:10 -0300

    update tela login e conserto fundo

--- public/logincadastro.js
+++ public/logincadastro.js
@@ -2,7 +2,8 @@ var botaoSignIn = document.querySelector("#signin")
 var botaoSignUp = document.querySelector("#signup")
 
 var body = document.querySelector("body")
-
+// chama a função onde a classe do body é alterada para sign in ou signup
+// e cada uma delas tem suas configurações e estilos no arquivo do css
 botaoSignIn.addEventListener("click", function(){
     body.className = "sign-in-js"
 })
