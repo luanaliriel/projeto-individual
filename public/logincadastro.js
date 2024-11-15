d3b63c8314edae0bc978f5fa7220532897c075fe
luanaliriel <luana.bsantos@sptech.school>
2024-11-15 00:50:35 -0300

    mudança do login/cadastro

--- public/logincadastro.js
+++ public/logincadastro.js
@@ -1,12 +1,13 @@
-var botaoSignIn = document.querySelector("#signin")
-var botaoSignUp = document.querySelector("#signup")
+const container = document.getElementById('container');
+const botaoRegistrar = document.getElementById('registrar');
+const botaoLogar = document.getElementById('login');
 
-var body = document.querySelector("body")
-// chama a função onde a classe do body é alterada para sign in ou signup
-// e cada uma delas tem suas configurações e estilos no arquivo do css
-botaoSignIn.addEventListener("click", function(){
-    body.className = "sign-in-js"
-})
-botaoSignUp.addEventListener("click", function(){
-    body.className = "sign-up-js"
-})
+// Registrar - ativa o modo de cadastro
+botaoRegistrar.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+// Logar - ativa o modo de login
+botaoLogar.addEventListener('click', () => {
+    container.classList.remove('active');
+});
