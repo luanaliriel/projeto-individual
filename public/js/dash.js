99177a4927521f154a85f16c80a46c4a7566e1a4
luanaliriel <luana.bsantos@sptech.school>
2024-11-26 01:46:05 -0300

    subindo projeto finalizado (graficos 2 e 3 prontos com api ligada)

--- public/js/dash.js
+++ public/js/dash.js
@@ -1,6 +1,18 @@
-// gráfico 1
+// vetores pro gráfico 1
 var listaPersonagens = []
 var listaContagem = []
+
+
+// vetores pro gráfico 2
+var listaPersonagensG2 = []
+var lista10a20 = []
+var lista21a30 = []
+var lista30plus = []
+
+// vetores pro gráfico 3
+var listaPersonagensG3 = []
+var generos = ['Feminino', 'Masculino']
+var contagens = { Feminino: [], Masculino: [] }
 
 
 function sairDaSessao() {
@@ -12,248 +24,340 @@ function sairDaSessao() {
 
 // CONTANDO TOTAL DE PARTICIPANTES
 
-    fetch("/pontuacao/contarTotalParticipantes", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(function (resposta) {
-            console.log("resposta: ", resposta);
+fetch("/pontuacao/contarTotalParticipantes", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+    .then(function (resposta) {
+        console.log("resposta: ", resposta);
 
-            if (resposta.ok) {
-                resposta.json().then(json => {
-                    console.log('json')
-                    console.log(json)
-                    valorTotalDeParticipantes.innerHTML = json[0].contagemTotal
-                })
-            } else {
-                console.log(resposta)
-            }
-        })
-        .catch(function (resposta) {
-            console.log(`#ERRO: ${resposta}`);
-        });
+        if (resposta.ok) {
+            resposta.json().then(json => {
+                console.log('json')
+                console.log(json)
+                valorTotalDeParticipantes.innerHTML = json[0].contagemTotal
+            })
+        } else {
+            console.log(resposta)
+        }
+    })
+    .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+    });
 
 
 // VERIFICANDO IDENTIFICAÇÃO COM PERSONAGEM
 
-    fetch("/pontuacao/verificarIdentificacaoPersonagem", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    })
-        .then(function (resposta) {
-            console.log("resposta: ", resposta);
+fetch("/pontuacao/verificarIdentificacaoPersonagem", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+    .then(function (resposta) {
+        console.log("resposta: ", resposta);
 
-            if (resposta.ok) {
-                resposta.json().then(json => {
-                    console.log('json')
-                    console.log(json)
-                    identificacaoPorPersonagem.innerHTML = json[0].personagemFinal
+        if (resposta.ok) {
+            resposta.json().then(json => {
+                console.log('json')
+                console.log(json)
+                identificacaoPorPersonagem.innerHTML = json[0].personagemFinal
 
-                    for (let index = 0; index < json.length; index++) {
-                        listaPersonagens.push(json[index].personagemFinal)
-                        listaContagem.push(json[index].contagem)
-                    }
-                    const meuchart = document.getElementById('chartJS')
+                for (var i = 0; i < json.length; i++) {
+                    const dadoGrafico1 = json[i]
+                    listaPersonagens.push(dadoGrafico1.personagemFinal)
+                    listaContagem.push(dadoGrafico1.contagem)
+                }
+                const meuchart = document.getElementById('chartJS')
 
-                    const data = {
-                        labels: listaPersonagens, /* EIXO X */
-                        datasets: [{
-                            label: `Quantidade`,
-                            backgroundColor: [`#ff35e1`, `#f6e569`, '#D9FFFC'],
-                            borderColor: 'transparent',
-                            data: listaContagem /* EIXO y */
+                const data = {
+                    labels: listaPersonagens, /* EIXO X */
+                    datasets: [{
+                        label: `Quantidade`,
+                        backgroundColor: [`#ff35e1`, `#f6e569`, '#D9FFFC'],
+                        borderColor: 'transparent',
+                        data: listaContagem /* EIXO y */
+                    },
+                    ]
+                }
+
+                const config = {
+                    type: 'pie',
+                    data: data,
+                    options: {
+                        plugins: {
+                            legend: {
+                                labels: {
+                                    font: {
+                                        size: 14,
+                                        weight: 'bold',
+                                        family: 'Poppins'
+                                    },
+                                    color: '#6d6d6d',
+                                },
+                            },
                         },
-                        ]
+                    },
+                }
+
+                new Chart(meuchart, config)
+
+
+
+            })
+        } else {
+            console.log(resposta)
+        }
+    })
+    .catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+    });
+
+
+
+
+
+
+
+
+nomeUsuario.innerHTML = sessionStorage.NOME_USUARIO
+idadeUsuario.innerHTML += sessionStorage.IDADE_USUARIO
+generoUsuario.innerHTML += sessionStorage.GENERO_USUARIO
+
+
+
+
+// GRÁFICO 2 - PREFERENCIA POR IDADE
+
+fetch("/pontuacao/verificarPersonagemPorFaixaEtaria", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+    .then(function (resposta) {
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+            resposta.json().then(json => {
+                console.log('json')
+                console.log(json)
+
+                for (var i2 = 0; i2 < json.length; i2++) {
+                    const dadoGrafico2 = json[i2]
+                    var posicao = -1
+
+                    for (var i3 = 0; i3 < listaPersonagensG2.length; i3++) {
+                        if (listaPersonagensG2[i3] == dadoGrafico2.personagemFinal) {
+                            posicao = i3
+                            break
+                        }
                     }
 
-                    const config = {
-                        type: 'pie',
-                        data: data,
-                        options: {
-                            plugins: {
-                                legend: {
-                                    labels: {
-                                        font: {
-                                            size: 14,
-                                            weight: 'bold',
-                                            family: 'Poppins'
-                                        },
-                                        color: '#6d6d6d',
+                    if (posicao == -1) {
+                        posicao = listaPersonagensG2.length
+                        listaPersonagensG2.push(dadoGrafico2.personagemFinal)
+                        lista10a20.push(0)
+                        lista21a30.push(0)
+                        lista30plus.push(0)
+                    }
+
+                    if (dadoGrafico2.faixaEtaria == '10-20') {
+                        lista10a20[posicao] = dadoGrafico2.contagem
+                    } else if (dadoGrafico2.faixaEtaria == '21-30') {
+                        lista21a30[posicao] = dadoGrafico2.contagem
+                    } else if (dadoGrafico2.faixaEtaria == '30++') {
+                        lista30plus[posicao] = dadoGrafico2.contagem
+                    }
+                }
+
+
+                const meuchart2 = document.getElementById('chartJS2')
+
+                const data2 = {
+                    labels: listaPersonagensG2,
+                    datasets: [
+                        {
+                            label: '10 a 20 anos',
+                            data: lista10a20,
+                            backgroundColor: '#ff35e1',
+                        },
+                        {
+                            label: '21-30 anos',
+                            data: lista21a30,
+                            backgroundColor: '#D9FFFC',
+                        },
+                        {
+                            label: '30++ anos',
+                            data: lista30plus,
+                            backgroundColor: '#5900c5',
+                        },
+                    ]
+                };
+
+                const config2 = {
+                    type: 'bar',
+                    data: data2,
+                    options: {
+                        responsive: true,
+                        plugins: {},
+                        scales: {
+                            x: {
+                                stacked: true,
+                                ticks: {
+                                    font: {
+                                        size: 14,
+                                        family: 'Poppins',
+                                        weight: 'bold',
+                                    },
+                                    color: '#6d6d6d',
+                                },
+                            },
+                            y: {
+                                stacked: true,
+                                title: {
+                                    display: true,
+                                    text: 'Número de Usuários',
+                                    font: {
+                                        size: 14,
+                                        family: 'Poppins',
+                                        weight: 'bold',
+                                    },
+                                    color: '#6d6d6d',
+                                },
+                                ticks: {
+                                    font: {
+                                        size: 14,
+                                        family: 'Poppins',
+                                        weight: 'bold',
+                                    }
+                                }
+                            },
+                        },
+                    },
+                }
+
+                new Chart(meuchart2, config2)
+            })
+        } else {
+            console.log("Erro na resposta: ", resposta);
+        }
+    })
+    .catch(erro => {
+        console.log(`#ERRO: ${erro}`);
+    });
+
+
+
+
+
+// GRÁFICO 3 - PREFERÊNCIA POR GÊNERO
+
+fetch("/pontuacao/verificarPersonagemPorGenero", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+})
+    .then((resposta) => {
+        if (resposta.ok) {
+            resposta.json().then((json) => {
+
+                for (let i4 = 0; i4 < json.length; i4++) {
+                    const { personagemFinal, genero, contagem } = json[i4];
+
+                    // coloca o personagem na lista se ele ainda nao estiver
+                    if (!listaPersonagensG3.includes(personagemFinal)) {
+                        listaPersonagensG3.push(personagemFinal);
+                    }
+
+                    // preenche a contagem para o respectivo genero e personagem
+                    if (generos.includes(genero)) {
+                        // pega a posicao do personagem
+                        const index = listaPersonagensG3.indexOf(personagemFinal);
+
+                        // armazena a contagem do genero na posicao correspondente
+                        contagens[genero][index] = contagens[genero][index] ? contagens[genero][index] + contagem : contagem;
+                    }
+                }
+
+                const meuchart3 = document.getElementById('chartJS3');
+
+                const data3 = {
+                    labels: listaPersonagensG3,
+                    datasets: [
+                        {
+                            label: "Feminino",
+                            data: contagens.Feminino,
+                            backgroundColor: "#ff35e1",
+                        },
+                        {
+                            label: "Masculino",
+                            data: contagens.Masculino,
+                            backgroundColor: "#44dc9c",
+                        },
+                    ],
+                };
+
+                const config3 = {
+                    type: "bar",
+                    data: data3,
+                    options: {
+                        responsive: true,
+                        plugins: {
+                            legend: {},
+                        },
+                        scales: {
+                            x: {
+                                title: {
+                                    display: true,
+                                    text: "",
+                                    font: {
+                                        size: 14,
+                                        weight: "bold",
+                                        family: "Poppins",
+                                    },
+                                    color: "#6d6d6d",
+                                },
+                                ticks: {
+                                    font: {
+                                        size: 14,
+                                        weight: "bold",
+                                        family: "Poppins",
+                                    },
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: "Número de Usuários",
+                                    font: {
+                                        size: 14,
+                                        family: "Poppins",
+                                        weight: "bold",
+                                    },
+                                    color: "#6d6d6d",
+                                },
+                                ticks: {
+                                    font: {
+                                        size: 14,
+                                        family: "Poppins",
+                                        weight: "bold",
                                     },
                                 },
                             },
                         },
-                    }
-
-                    new Chart(meuchart, config)
-
-
-
-                })
-            } else {
-                console.log(resposta)
-            }
-        })
-        .catch(function (resposta) {
-            console.log(`#ERRO: ${resposta}`);
-        });
-
-
-
-
-
-
-
-
-    nomeUsuario.innerHTML = sessionStorage.NOME_USUARIO
-    idadeUsuario.innerHTML += sessionStorage.IDADE_USUARIO
-    generoUsuario.innerHTML += sessionStorage.GENERO_USUARIO
-
-
-
-
-    // GRÁFICO 2 - PREFERENCIA POR IDADE
-
-    const meuchart2 = document.getElementById('chartJS2')
-
-    const data2 = {
-        labels: ['Shizuku', 'Seiji', 'Mistura'],
-        datasets: [
-            {
-                label: '10 a 20 anos',
-                data: [15, 3, 2],
-                backgroundColor: '#ff35e1',
-            },
-            {
-                label: '21-30 anos',
-                data: [9, 4, 2],
-                backgroundColor: '#D9FFFC',
-            },
-            {
-                label: '30++ anos',
-                data: [5, 17, 10],
-                backgroundColor: '#5900c5',
-            },
-        ]
-    };
-
-    const config2 = {
-        type: 'bar',
-        data: data2,
-        options: {
-            responsive: true,
-            plugins: {},
-            scales: {
-                x: {
-                    stacked: true,
-                    ticks: {
-                        font: {
-                            size: 14,
-                            family: 'Poppins',
-                            weight: 'bold',
-                        },
-                        color: '#6d6d6d',
                     },
-                },
-                y: {
-                    stacked: true,
-                    title: {
-                        display: true,
-                        text: 'Número de Usuários',
-                        font: {
-                            size: 14,
-                            family: 'Poppins',
-                            weight: 'bold',
-                        },
-                        color: '#6d6d6d',
-                    },
-                    ticks: {
-                        font: {
-                            size: 14,
-                            family: 'Poppins',
-                            weight: 'bold',
-                        }
-                    }
-                },
-            },
-        },
-    }
+                };
 
-    new Chart(meuchart2, config2)
-
-    // GRÁFICO 3 - PREFERENCIA POR GÊNERO
-
-    const meuchart3 = document.getElementById('chartJS3');
-
-    const data3 = {
-        labels: ['Shizuku', 'Seiji'],
-        datasets: [
-            {
-                label: 'Feminino',
-                data: [40, 20],
-                backgroundColor: '#ff35e1',
-            },
-            {
-                label: 'Masculino',
-                data: [25, 35],
-                backgroundColor: '#44dc9c',
-            },
-        ]
-    };
-
-    const config3 = {
-        type: 'bar',
-        data: data3,
-        options: {
-            responsive: true,
-            plugins: {
-                legend: {},
-            },
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Personagens',
-                        font: {
-                            size: 14,
-                            weight: 'bold',
-                            family: 'Poppins',
-                        },
-                        color: '#6d6d6d',
-                    },
-                    ticks: {
-                        font: {
-                            size: 14,
-                            weight: 'bold',
-                            family: 'Poppins',
-                        },
-                    },
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Número de Usuários',
-                        font: {
-                            size: 14,
-                            family: 'Poppins',
-                            weight: 'bold',
-                        },
-                        color: '#6d6d6d',
-                    },
-                    ticks: {
-                        font: {
-                            size: 14,
-                            family: 'Poppins',
-                            weight: 'bold',
-                        }
-                    }
-                },
-            },
-        },
-    }
-
-    new Chart(meuchart3, config3)
+                new Chart(meuchart3, config3);
+            });
+        } else {
+            console.error("Erro ao buscar dados do gráfico por gênero:", resposta);
+        }
+    })
+    .catch((erro) => {
+        console.error(`#ERRO: ${erro}`);
+    });
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,62 +1,128 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE sussurrosDoCoracao;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
-);
+USE sussurrosDoCoracao;
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+idUsuario int primary key auto_increment,
+nome varchar(45),
+senha varchar(45),
+email varchar(45) unique, 
+idade int,
+genero varchar(10), constraint chkGenero check (genero in('Feminino', 'Masculino'))
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE pontuacao (
+idPontuacao int primary key auto_increment,
+pontosShizuku int,
+pontosSeiji int,
+personagemFinal varchar(15), constraint chkPersonagem check (personagemFinal in('Shizuku', 'Seiji', 'Mistura')),
+fkUsuario int, foreign key (fkUsuario) references usuario(idUsuario)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+SELECT * from usuario;
+SELECT * from pontuacao;
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+INSERT INTO usuario (nome, senha, email, idade, genero) VALUES
+('Luana Liriel', 'Luana123@', 'luhliriel@gmail.com', 22, 'Feminino'),
+('Jorge Uliam', 'Jorge123#', 'jorge.uliam@gmail.com', 23, 'Masculino'),
+('Amanda Silva', 'Amanda456$', 'amanda.silva@gmail.com', 27, 'Feminino'),
+('Carlos Eduardo', 'Carlos789%', 'carlos.edu@gmail.com', 30, 'Masculino'),
+('Julia Souza', 'Julia321&', 'julia.souza@gmail.com', 19, 'Feminino'),
+('Marcos Vinícius', 'Marcos654*', 'marcos.vini@gmail.com', 34, 'Masculino'),
+('Ana Paula', 'Ana987@', 'ana.paula@gmail.com', 28, 'Feminino'),
+('Rafael Alves', 'Rafael246!', 'rafael.alves@gmail.com', 23, 'Masculino'),
+('Beatriz Lima', 'Beatriz135%', 'beatriz.lima@gmail.com', 21, 'Feminino'),
+('Gabriel Costa', 'Gabriel852&', 'gabriel.costa@gmail.com', 49, 'Masculino'),
+('Mariana Ferreira', 'Mari753#', 'mariana.ferreira@gmail.com', 24, 'Feminino'),
+('Lucas Oliveira', 'Lucas369$', 'lucas.oliveira@gmail.com', 29, 'Masculino'),
+('Isabela Rocha', 'Isa159@', 'isabela.rocha@gmail.com', 22, 'Feminino'),
+('Pedro Henrique', 'Pedro951%', 'pedro.henrique@gmail.com', 31, 'Masculino'),
+('Fernanda Gomes', 'Fernanda753&', 'fernanda.gomes@gmail.com', 56, 'Feminino'),
+('Thiago Martins', 'Thiago357*', 'thiago.martins@gmail.com', 27, 'Masculino'),
+('Carolina Santos', 'Carol258!', 'carol.santos@gmail.com', 19, 'Feminino'),
+('Vinícius Mendes', 'Vini654@', 'vinicius.mendes@gmail.com', 33, 'Masculino'),
+('Letícia Andrade', 'Leti369$', 'leticia.andrade@gmail.com', 23, 'Feminino'),
+('Fábio Monteiro', 'Fabio753%', 'fabio.monteiro@gmail.com', 28, 'Masculino'),
+('Lara Campos', 'Lara135&', 'lara.campos@gmail.com', 58, 'Feminino'),
+('Bruno Silva', 'Bruno987*', 'bruno.silva@gmail.com', 26, 'Masculino'),
+('Camila Ribeiro', 'Camila123!', 'camila.ribeiro@gmail.com', 30, 'Feminino'),
+('Gustavo Moraes', 'Gustavo456@', 'gustavo.moraes@gmail.com', 61, 'Masculino'),
+('Patrícia Lopes', 'Patricia789$', 'patricia.lopes@gmail.com', 25, 'Feminino'),
+('Eduardo Nascimento', 'Edu135%', 'eduardo.nascimento@gmail.com', 31, 'Masculino'),
+('Sabrina Oliveira', 'Sabrina852&', 'sabrina.oliveira@gmail.com', 69, 'Feminino'),
+('Ricardo Barbosa', 'Ricardo951*', 'ricardo.barbosa@gmail.com', 32, 'Masculino'),
+('Natália Cunha', 'Natalia357!', 'natalia.cunha@gmail.com', 22, 'Feminino'),
+('Felipe Cardoso', 'Felipe654@', 'felipe.cardoso@gmail.com',67, 'Masculino'),
+('Jorge Ulian', 'jorji123@', 'jorjiomg@gmail.com', 19, 'Masculino'),
+('Filipa Urias', 'Filipa@', 'filipau@gmail.com',12, 'Feminino'),
+('Carlos Emanuel', 'carloseman123#', 'carlosem3@gmail.com', 67, 'Masculino');
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
 
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
+
+INSERT INTO pontuacao (pontosShizuku, pontosSeiji, personagemFinal, fkUsuario) VALUES
+(7, 3, 'Shizuku', 1), -- Luana Liriel (22 anos, jovem)
+(3, 7, 'Shizuku', 2),   -- Jorge Uliam (23 anos, mais jovem)
+(6, 4, 'Shizuku', 3), -- Amanda Silva (27 anos, jovem)
+(4, 6, 'Seiji', 4),   -- Carlos Eduardo (30 anos, jovem com equilíbrio)
+(8, 2, 'Shizuku', 5), -- Julia Souza (19 anos, muito jovem)
+(2, 8, 'Seiji', 6),   -- Marcos Vinícius (34 anos, mais velho)
+(7, 3, 'Shizuku', 7), -- Ana Paula (28 anos, jovem)
+(6, 4, 'Shizuku', 8), -- Rafael Alves (23 anos, jovem)
+(8, 2, 'Shizuku', 9), -- Beatriz Lima (21 anos, jovem)
+(2, 8, 'Seiji', 10),  -- Gabriel Costa (49 anos, mais velho)
+(7, 3, 'Shizuku', 11),-- Mariana Ferreira (24 anos, jovem)
+(5, 5, 'Mistura', 12),-- Lucas Oliveira (29 anos, equilíbrio)
+(8, 2, 'Shizuku', 13),-- Isabela Rocha (22 anos, jovem)
+(4, 6, 'Seiji', 14),  -- Pedro Henrique (31 anos, mais velho)
+(3, 7, 'Seiji', 15),  -- Fernanda Gomes (56 anos, mais velho)
+(6, 4, 'Shizuku', 16),-- Thiago Martins (27 anos, jovem)
+(8, 2, 'Shizuku', 17),-- Carolina Santos (19 anos, muito jovem)
+(4, 6, 'Seiji', 18),  -- Vinícius Mendes (33 anos, mais velho)
+(7, 3, 'Shizuku', 19),-- Letícia Andrade (23 anos, jovem)
+(6, 4, 'Shizuku', 20),-- Fábio Monteiro (28 anos, jovem)
+(3, 7, 'Seiji', 21),  -- Lara Campos (58 anos, mais velho)
+(6, 4, 'Shizuku', 22),-- Bruno Silva (26 anos, jovem)
+(5, 5, 'Mistura', 23),-- Camila Ribeiro (30 anos, equilíbrio)
+(3, 7, 'Seiji', 24),  -- Gustavo Moraes (61 anos, mais velho)
+(7, 3, 'Shizuku', 25),-- Patrícia Lopes (25 anos, jovem)
+(4, 6, 'Seiji', 26),  -- Eduardo Nascimento (31 anos, mais velho)
+(2, 8, 'Seiji', 27),  -- Sabrina Oliveira (69 anos, muito velho)
+(4, 6, 'Seiji', 28),  -- Ricardo Barbosa (32 anos, mais velho)
+(7, 3, 'Shizuku', 29),-- Natália Cunha (22 anos, jovem)
+(3, 7, 'Seiji', 30),  -- Felipe Cardoso (67 anos, muito velho)
+(4, 6, 'Seiji', 31),
+(5, 5, 'Mistura', 32),
+(5, 5, 'Mistura', 33);
+
+
+-- KPI: total de participantes
+SELECT count(*) AS contagemTotal FROM pontuacao;
+
+
+-- KPI: Maioria dos usuários se identifica mais com
+SELECT personagemFinal, count(*) as contagem
+FROM pontuacao
+GROUP BY 1
+ORDER BY count(*) DESC;
+
+
+-- Gráfico: Personagem por faixa etária
+SELECT personagemFinal,
+	CASE 
+        WHEN idade BETWEEN 10 AND 20 THEN '10-20'
+        WHEN idade BETWEEN 21 AND 30 THEN '21-30'
+        WHEN idade > 30 THEN '30++'
+    END AS faixaEtaria,
+    count(*) as contagem
+    from pontuacao join usuario
+    on idUsuario = fkUsuario
+    group by 1,2 order by personagemFinal desc;
+    
+
+
+-- Gráfico: Personagem por gênero
+SELECT personagemFinal, genero, count(*) as contagem
+from pontuacao inner join usuario
+on idUsuario = fkUsuario
+group by 1,2
+order by personagemFinal desc;
